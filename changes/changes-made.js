8f3e2dac5f0bfa88bd65f873544ce1824a752989
willington.mnisi@gmil.com <willington.mnisi@gmail.com>
2020-09-23 16:10:07 +0200

    resized left menu on media screen

--- changes/changes-made.js
+++ changes/changes-made.js
@@ -1,5 +1,6 @@
 Landing page scss
 1. changed line-height and height from 85 to 50 (ln 280, 283)
 2. added web-title id, and set margin to 0 (ln 658)
-3. #right and #left after ln 259
+3. set z index to #history-items, #inventory-items, #pending-items, and resized them on media screen
 4. #mid hidden in 249
+
